27034eab43a8dd7fccfe9dd0e2e2147593a8d348
Paul Arthur Weinberger <salmones22@gmail.com>
2019-01-15 19:14:11 +0000

    INITIAL COMMIT

---
+++ Infrastructure/Connector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Basically.Models;
+using LiteDB;
+
+namespace Basically.Infrastructure
+{
+    public class Connector
+    {
+        private IConfiguration configuration;
+        public Connector(IConfiguration _configuration) {
+            configuration = _configuration;
+        }
+   
+        public LiteCollection<T> List<T>() {
+            using (var db = new LiteDatabase(configuration.GetSection("Settings").GetSection("LiteDBFile").Value))
+            {
+                return db.GetCollection<T>(typeof(T).ToString());
+            }
+        }
+
+        public string Set() {
+            return "";
+        }
+
+        public string Get() {
+            return "";
+        }
+
+        public string Update() {
+            return "";
+        }
+    }
+}
---
+++ Models/Site.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basically.Models
+{
+    public class Site
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[] Cultures { get; set; }
+    }
+}
